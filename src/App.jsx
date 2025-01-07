29ff528cb59d95084155684acaf3394043acb1b6
Eric Grebe <grinsikp@littlebottom.de>
2025-01-07 08:35:36 +0100

    Change index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
   return (
-    <>
+    <div className="mx-auto lg:max-w-7xl">
       <main>
         <h1 className="text-3xl">Pomotask</h1>
       </main>
-    </>
+    </div>
   );
 }
 
