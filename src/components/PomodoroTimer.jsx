dcb1a783c97dbc3c2f65ec48a93e082df8f05151
Eric Grebe <grinsikp@littlebottom.de>
2025-01-09 11:18:55 +0100

    Add controle icons

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -1,7 +1,7 @@
 export default function PomodoroTimer() {
   return (
-    <div className="mx-auto w-2/5 border-4 border-white border-opacity-80 rounded-lg py-2 px-2">
-      <div id="timerMenuBar">
+    <div className="mx-auto w-2/5 py-2 px-2 bg-blue-600 border-4 border-white border-opacity-80 rounded-lg">
+      <div id="timerMenuBar" className="mb-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -23,9 +23,53 @@ export default function PomodoroTimer() {
         </svg>
       </div>
       <div id="timerDisplay">
-        <p>00:00</p>
+        <p className="text-8xl text-center font-bold">25:00</p>
       </div>
-      <div id="timerControls">play</div>
+      <div id="timerControls" className="mt-4 mb-2 flex justify-center gap-5 ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-9 opacity-75 hover:opacity-100 hover:cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="size-9 opacity-75 hover:opacity-100 hover:cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="size-9 opacity-75 hover:opacity-100 hover:cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
