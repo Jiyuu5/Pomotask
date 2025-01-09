2a51be68f0e768414d8c4ab8091022217fd8c5e9
Eric Grebe <grinsikp@littlebottom.de>
2025-01-09 09:53:43 +0100

    Add components folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
+import PomodoroTimer from "./PomodoroTimer";
+
 export default function App() {
   return (
     <div className="mx-auto lg:max-w-7xl">
       <main>
         <h1 className="text-5xl text-center">Pomotask</h1>
+        <PomodoroTimer />
       </main>
     </div>
   );
