1f68b961bfbb6db7b983586265b9f301809f86cb
Eric Grebe <grinsikp@littlebottom.de>
2025-01-10 08:55:36 +0100

    Add resetTimer function

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 
 export default function PomodoroTimer() {
-  const [minutesTimerWork, setMinutesTimerWork] = useState(25);
-  const [secondsTimerWork, setSecondsTimerWork] = useState(0);
+  const [currentTimer, setCurrentTimer] = useState(605);
+  const [workTimer, setWorkTimer] = useState(1500);
+
+  const minutes = Math.round(currentTimer / 60);
+  const seconds = currentTimer % 60;
+
+  function resetTimer() {
+    setCurrentTimer(workTimer);
+  }
 
   return (
     <div className="mx-auto w-2/5 py-4 px-4 bg-blue-600 border-4 border-white border-opacity-80 rounded-lg">
@@ -29,14 +36,12 @@ export default function PomodoroTimer() {
       </div>
       <div id="timerDisplay">
         <p className="text-8xl text-center font-bold">
-          {minutesTimerWork}:
-          {secondsTimerWork < 10
-            ? "0" + secondsTimerWork.toString()
-            : secondsTimerWork}
+          {minutes}:{seconds < 10 ? "0" + seconds.toString() : seconds}
         </p>
       </div>
       <div id="timerControls" className="mt-8 mb-2 flex justify-center gap-5 ">
         <svg
+          onClick={resetTimer}
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -50,7 +55,6 @@ export default function PomodoroTimer() {
             d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
           />
         </svg>
-
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
